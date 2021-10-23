022e93dfb6a6fbd50b55feed71fb32924d173b94
Leonardo Nascimento <leonardo.nascimentoo02@hotmail.com>
2021-10-23 11:14:19 -0300

    test.py

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
-# -*- coding: utf-8 -*-                                                                                                                                                                                  
-from app import app                                                                                                                                                                                      
-import unittest                                                                                                                                                                                          
+# -*- coding: utf-8 -*-
+from app import app
+import unittest
 
-class Test(unittest.TestCase):                                                                                                                                                                           
+class Test(unittest.TestCase):
+    def setUp(self):
+        # cria uma instância do unittest, precisa do nome "setUp"
+        self.app = app.test_client()
 
-    def setUp(self):                                                                                                                                                                                     
-        # cria uma instância do unittest, precisa do nome "setUp"                                                                                                                                        
-        self.app = app.test_client()                                                                                                                                                                     
+        # envia uma requisicao GET para a URL
+        self.result = self.app.get('/')
 
-        # envia uma requisicao GET para a URL                                                                                                                                                            
-        self.result = self.app.get('/')                                                                                                                                                                  
+        # envia uma requisicao GET para a URL + endpoint /soma
+        self.resultSoma = self.app.get('/soma')
 
-    def test_requisicao(self):                                                                                                                                                                           
-        # compara o status da requisicao (precisa ser igual a 200)                                                                                                                                       
-        self.assertEqual(self.result.status_code, 200)                                                                                                                                                   
+    def test_requisicao(self):
+        # compara o status da requisicao (precisa ser igual a 200)
+        self.assertEqual(self.result.status_code, 200)
 
-    def test_conteudo(self):                                                                                                                                                                             
-        # verifica o retorno do conteudo da pagina                                                                                                                                                       
+        # compara o status da requisicao para o endpoint /soma (precisa ser igual a 200)
+        self.assertEqual(self.resultSoma.status_code, 200)
+
+    def test_conteudo(self):
+        # verifica o retorno do conteudo da pagina
         self.assertEqual(self.result.data.decode('utf-8'), "Funcionou :)")
+
+        # verifica o retorno do conteudo da pagina /soma
+        self.assertEqual(self.resultSoma.data.decode('utf-8'), "Sua soma de 10+10=20")
